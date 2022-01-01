e2bb2b739dab82b2772764da6fb0778001337c3e
jieun yoo <jyoo49@uic.edu>
2022-01-01 15:10:44 -0500

    add test that a candidate gets a vote from a voter

--- test/election.js
+++ test/election.js
@@ -29,6 +29,8 @@ contract("Election", accounts => {
 		assert.equal(candidate[1], "Candidate 1", "contains the correct name");
 	});
 
+
+	//this tests that a voter from account[0] votes for candidate 2, and that now voter 1 has voted once
 	it("checks that a voter can vote", async () => {
 		const instance = await Election.deployed();
 		const voted = await instance.vote(2, {from:accounts[0]});
@@ -36,6 +38,21 @@ contract("Election", accounts => {
 		assert.equal(voterResult, 1, "voter 1 voted");
 
 	});
+
+	//this test that voter from account[1] voted for candidate 1, and candidate 1 now has just 1 vote
+	it("checks that a candidate got a vote from a voter", async () => {
+		const instance = await Election.deployed();
+		const voted = await instance.vote(1, {from:accounts[1]});
+		const candidate = await instance.candidates(1);
+		assert.equal(candidate[3], 1, "got a vote");
+
+	});
+
+
+
+
+
+
 });
 
 
